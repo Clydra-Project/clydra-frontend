3e008e0ec56bf5d3c854abb70f951d77e597cb85
r00tsyst3m <muliawan.daffa@gmail.com>
2022-08-23 22:15:54 +0700

    CP-4 test2

---
+++ src/module/nav/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav(): any {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Test2
+    </h1>
+  );  
+}
+
+export default Nav;
